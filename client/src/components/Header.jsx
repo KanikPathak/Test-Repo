88cb9564c0427b87b62236bcff0788b0e8bffa38
Aditya Kumar Singh <adityakumarsingh1403@gmail.com>
2024-02-10 14:40:38 +0530

    navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,51 @@
- import React,{useState} from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import { FaBars } from "react-icons/fa";
-import {AiOutlineClose} from "react-icons/ai"
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
+function Header() {
 
-const Header = () => {
-const[isNavShowing, setIsNavShowing] = useState(window.innerWidth ? true : false);
+  const [collapsed, setCollapsed] = useState(true);
 
-const closeNavhandler = () => {
-  if(window.innerWidth < 800) {
-    setIsNavShowing(false);
-  } else{
-    setIsNavShowing(true)
-  }
-}
+    const toggleNavbar = () => {
+        setCollapsed(!collapsed);
+    };
 
+    const handleLinkClick = () => {
+        if (!collapsed) {
+            toggleNavbar();
+        }
+    };
 
   return (
-    
-    <nav>
-      <div className='container nav__container'>
-        <Link to="/" className='nav__logo' onClick={closeNavhandler}>
-          <img src='{Logo}' alt='Navbar Logo'/>
-        </Link>
-        {isNavShowing && <ul className='nav__menu'>
-          <li><Link to="/profile/sdfsdf" onClick={closeNavhandler}>Random name</Link></li>
-          <li><Link to="/create" onClick={closeNavhandler}>Create Post</Link></li>
-          <li><Link to="/authors" onClick={closeNavhandler}>Authors</Link></li>
-          <li><Link to="/logout" onClick={closeNavhandler}>Logout</Link></li>
-        </ul>}
-        <button className='nav__toggle-btn' onClick={() =>setIsNavShowing(!isNavShowing)}>
-        { isNavShowing ? <AiOutlineClose/> : <FaBars/>}
-        </button>
+    <nav className="navbar navbar-expand-lg">
+            <button className="navbar-toggler" type="button" onClick={toggleNavbar} aria-controls="navbarSupportedContent" aria-expanded={!collapsed} aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M1.5 4.5A.5.5 0 0 1 2 4h12a.5.5 0 0 1 0 1H2a.5.5 0 0 1-.5-.5zM1.5 8a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 0 1H2a.5.5 0 0 1-.5-.5zm0 3.5a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 0 1H2a.5.5 0 0 1-.5-.5z"/>
+                  </svg>
+            </span>
+            </button>
 
-      </div>
-    </nav>      
-
-  )
+            <div className={`collapse navbar-collapse ${collapsed ? '' : 'show'}`} id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/" onClick={handleLinkClick}>Home <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create" onClick={handleLinkClick}>Create Post</Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav my-2 my-lg-0">
+                    <li className="nav-item my-2 my-sm-0">
+                        <Link className="nav-link" to="/login" onClick={handleLinkClick} id="login-nav">Login</Link>
+                    </li>
+                    <li className="nav-item mr-sm-2">
+                        <Link className="nav-link" to="/signup" onClick={handleLinkClick} id="Signup-nav">Sign up</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+  );
 }
-
 export default Header;

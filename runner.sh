7225504089c00ec67f6dc929ce442a245b17fd18
Abhishek Gupta <abhigpta19@gmail.com>
2024-02-10 23:36:07 +0530

    deploy

---
+++ runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run server using nodemon
+gnome-terminal --tab --title="Server" --command="bash -c 'echo Running Server...; cd server && npm i && nodemon index.js ; exec bash'" &
+
+# Wait for a moment to let the server start (adjust if needed)
+sleep 2
+
+# Run client using npm start
+gnome-terminal --tab --title="Client" --command="bash -c 'echo Running Client...; cd client && npm i && npm start ; exec bash'" &
